c382f7b309892e43655dba8746d45e6321a7ac70
Ranielle Maglaqui <ranielle.maglaqui.cics@ust.edu.ph>
2023-11-08 18:22:14 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Http\Controllers\CategoryController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        $users = User::all();
+        return view('dashboard', compact ('users'));
+    })->name('dashboard');
+});
+
+Route::get('/all/categoty',[CategoryController::class,'index'])->name('AllCat');
+Route::get('/categories', [CategoryController::class, 'index'])->name('AllCat');
+Route::post('/categories', [CategoryController::class, 'store'])->name('AllCat');
+
